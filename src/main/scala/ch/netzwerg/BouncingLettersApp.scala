70cc43ae217a7fa7ef9ed10af470988663c6a675
Rahel Lüthy <rahel.luethy@gmail.com>
2016-03-23 15:14:21 +0100

    Place letters inside balls

--- src/main/scala/ch/netzwerg/BouncingLettersApp.scala
+++ src/main/scala/ch/netzwerg/BouncingLettersApp.scala
@@ -11,9 +11,19 @@ import scalatags.JsDom.all._
 
 object BouncingLettersApp extends js.JSApp {
 
-  def main(): Unit = {
+  val BallRadius = 50
+  val BallOffset = 10
+  val Height = 400
 
-    val height = 400
+  def translate(x: Int, y: Int) = s"translate($x,$y)"
+
+  def xPos(i: Int): Int = {
+    BallRadius + (i * (2 * BallRadius + BallOffset))
+  }
+
+  def yPos = Height - BallRadius - 5
+
+  def main(): Unit = {
 
     val inputField = input(
       `type` := "text",
@@ -23,34 +33,43 @@ object BouncingLettersApp extends js.JSApp {
 
     document.body.appendChild(inputField)
 
-    val svg = d3.select("body").append("svg").attr("width", "100%").attr("height", height)
+    val svg = d3.select("body").append("svg").attr("width", "100%").attr("height", Height)
 
     inputField.onkeyup = (e: dom.Event) => {
-      val data = inputField.value.map(c => c.toString).seq
+      val data = inputField.value.map(c => c.toString.toUpperCase).seq
 
       svg.selectAll("*").remove()
 
-      val text = svg.selectAll("text").data(data.toJSArray)
+      val balls = svg.selectAll("g").data(data.toJSArray)
 
-      text.enter().append("text")
-        .style("font-size", "x-large")
-        .attr("class", (s: String, i: Int) => i)
-        .attr("text-anchor", "start")
-        .attr("y", height / 2)
-        .attr("dx", (s: String, i: Int) => (i + 1) + "em")
-        .text((s: String) => s)
-        .on("mouseover", bounce)
+      val ballsEnter = balls.enter().append("g")
+        .attr("transform", (s: String, i: Int) => translate(xPos(i), yPos))
 
       def bounce: (String, Int) => Unit = (s, index) => {
-        text.filter((s: String, i: Int) => i == index).transition()
+        ballsEnter.filter((s: String, i: Int) => i == index).transition()
           .duration(300)
           .ease("cubic-out")
-          .attr("transform", "translate(0,-30)")
+          .attr("transform", translate(xPos(index), BallRadius))
           .transition()
           .duration(500)
           .ease("cubic-in")
-          .attr("transform", "translate(0,0)")
+          .attr("transform", translate(xPos(index), yPos))
       }
+
+      ballsEnter.on("mouseover", bounce)
+
+      val colors = d3.scale.category20().range()
+      ballsEnter.append("circle")
+        .attr("r", BallRadius)
+        .attr("fill", (s: String, i: Int) => colors(i))
+        .attr("stroke", "black")
+
+      ballsEnter.append("text")
+        .style("font-size", "x-large")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text((s: String) => s)
+
     }
 
   }
